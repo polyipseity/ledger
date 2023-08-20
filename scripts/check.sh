2b9f832fdfe20284b9723b1a17e7c72fcee73abb
William So <polyipseity@gmail.com>
2023-08-20 19:46:21 +0800

    Remove check `uniqueleafnames`
    Signed-off-by: William So <polyipseity@gmail.com>

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-hledger --file '../index.journal' --strict check accounts assertions balancednoautoconversion commodities ordereddates parseable payees tags uniqueleafnames
+hledger --file '../index.journal' --strict check accounts assertions balancednoautoconversion commodities ordereddates parseable payees tags
