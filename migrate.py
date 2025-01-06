bf288b55386711614d9214d56252ee34e498b682
William So <polyipseity@gmail.com>
2025-01-06 23:35:35 +0800

    Rename journals, step 3
    Signed-off-by: William So <polyipseity@gmail.com>

--- migrate.py
+++ migrate.py
@@ -1,25 +1,19 @@
 from glob import iglob
-from os import renames
 from pathlib import Path
 
 
 def main():
     for journal in iglob(
-        "**/index.journal",
+        "**/*.journal",
         root_dir=".",
         recursive=True,
     ):
-        comps = journal.split("\\")
-        name = comps[0]
-        if len(comps) >= 3:
-            year = comps[1]
+        text = Path(journal).read_text()
+        if "../prelude.journal" in text:
+            name = journal.split("\\")[-1].removesuffix(".journal")
             Path(journal).write_text(
-                Path(journal).read_text().replace(".journal", f"/{name}.journal")
+                text.replace("../prelude.journal", f"../../../preludes/{name}.journal")
             )
-            renames(journal, f"ledger/{year}/{name}.journal")
-        else:
-            renames(journal, f"ledger/{name}.journal")
-            # edit the text yourself
 
 
 if __name__ == "__main__":
